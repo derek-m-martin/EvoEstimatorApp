96823f6aef1a5cf3b505ac7ecd233264bb938d22
derek-m-martin <derekmartin1005@gmail.com>
2024-08-18 20:25:39 -0700

    Initial Commit

---
+++ EvoEstimator/EvoEstimatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvoEstimatorApp.swift
+//  EvoEstimator
+//
+//  Created by Derek Martin on 2024-08-18.
+//
+
+import SwiftUI
+
+@main
+struct EvoEstimatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
