8580dc114ea32a038b85b15fa14e52b7d28b63ab
derek-m-martin <derekmartin1005@gmail.com>
2024-10-11 17:39:23 -0700

    Created ComputeTripCost.swift to handle the calculation of how much the trip will cost

---
+++ EvoEstimator/ComputeTripCost.swift
@@ -0,0 +1,43 @@
+//
+//  ComputeTripCost.swift
+//  EvoEstimator
+//
+//  Created by Derek Martin on 2024-10-11.
+//
+
+import Foundation
+
+var perMin: Double = 0.49
+var perTripCharge: Double = 1.25
+var hourlyCharge: Double = 17.99
+var dailyCharge = 104.99
+var total = 0.0
+
+func calculateCost(travelCost: Double, completion: @escaping (Double) -> Void) {
+    let inMinutes = travelCost/60
+    if (inMinutes >= 37 && inMinutes <= 60) {
+        total = hourlyCharge
+    }
+    else if (inMinutes > 60 && inMinutes < 360) {
+        let numHours = inMinutes/60
+        let remaining = inMinutes - (numHours * 60)
+        let step1 = (numHours * hourlyCharge)
+        if (remaining < 37) {
+            total = step1 + (remaining * perMin)
+        }
+        else {
+            total = step1 + hourlyCharge
+        }
+    }
+    else if (inMinutes >= 360) {
+        total = dailyCharge
+    }
+    else {
+        total = (inMinutes * perMin)
+    }
+    
+    DispatchQueue.main.async {
+        completion(total)
+    }
+}
+
