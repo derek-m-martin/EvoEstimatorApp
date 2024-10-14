859276b25d603b8aea9780bd6badcdb10eae66b5
derek-m-martin <derekmartin1005@gmail.com>
2024-10-13 22:49:40 -0700

    Created AboutPage.swift and PrivacyPolicy.swift files which will be implemented at a future date

---
+++ EvoEstimator/PrivacyPolicy.swift
@@ -0,0 +1,18 @@
+//
+//  PrivacyPolicy.swift
+//  EvoEstimator
+//
+//  Created by Derek Martin on 2024-10-13.
+//
+
+import SwiftUI
+
+struct PrivacyPolicy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrivacyPolicy()
+}
