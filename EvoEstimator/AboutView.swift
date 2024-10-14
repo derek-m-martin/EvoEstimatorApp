1e6dc022b19b244079cfb3215490afc81aa7d3e9
derek-m-martin <derekmartin1005@gmail.com>
2024-10-14 16:13:40 -0700

    Changed struct name from 'AboutPage' to 'AboutView'

--- EvoEstimator/AboutView.swift
+++ EvoEstimator/AboutView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct AboutPage: View {
+struct AboutView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    AboutPage()
+    AboutView()
 }
