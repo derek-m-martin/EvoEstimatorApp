39c12261ad2aa02f367910788f0b436d228ad544
derek-m-martin <derekmartin1005@gmail.com>
2024-10-13 22:19:11 -0700

    Added the individual parts of the appicon to be used for the splash screen

---
+++ EvoEstimator/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  EvoEstimator
+//
+//  Created by Derek Martin on 2024-10-12.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
