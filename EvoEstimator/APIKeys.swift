288ddd2de781ad1057435fa13dc25b57cde56b5f
derek-m-martin <derekmartin1005@gmail.com>
2025-01-02 13:32:04 -0500

    API Key push since removed off gitignore

---
+++ EvoEstimator/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  EvoEstimator
+//
+//  Created by Derek Martin on 2024-10-11.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googleAPIKey = "AIzaSyAiaAJysr8xpMmGlc8O0hW3FIltArsPuQw"
+}
